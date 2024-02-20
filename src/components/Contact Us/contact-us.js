11edf47c120eb317857fba184e7d786af4999b36
Abinash <avinashniure026@gmail.com>
2024-02-20 15:54:48 +0930

    change

--- src/components/Contact Us/contact-us.js
+++ src/components/Contact Us/contact-us.js
@@ -36,124 +36,36 @@ const ContactUs = () => {
               title="Get In Touch"
               description="We thrive when coming up with innovative ideas but also understand that a smart concept should be supported with faucibus sapien odio measurable results."
             />
-
-            <Row>
-              <Col lg="4">
-                <div className="mt-4 pt-4">
-                  <p className="mt-4">
-                    <span className="h5">Office Address 1:</span>
-                    <br />{" "}
-                    <span className="text-muted d-block mt-2">
-                      4461 Cedar Street Moro, AR 72368
-                    </span>
-                  </p>
-                  <p className="mt-4">
-                    <span className="h5">Office Address 2:</span>
-                    <br />{" "}
-                    <span className="text-muted d-block mt-2">
-                      2467 Swick Hill Street <br />
-                      New Orleans, LA 70171
-                    </span>
-                  </p>
-                  <p className="mt-4">
-                    <span className="h5">Working Hours:</span>
-                    <br />{" "}
-                    <span className="text-muted d-block mt-2">
-                      9:00AM To 6:00PM
-                    </span>
-                  </p>
-                </div>
-              </Col>
-              <Col lg="8">
-                <div className="custom-form mt-4 pt-4">
-                <p id="error-msg"></p>
-                  <div id="message"></div>
-                  <Form
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      validation.handleSubmit();
-                      return false;
-                  }}>
-                    <Row>
-                      <Col lg="6 mt-2">
-                      <Input
-                          name="name"
-                          className=""
-                          placeholder="Your name*"
-                          type="text"
-                          onChange={validation.handleChange}
-                          onBlur={validation.handleBlur}
-                          value={validation.values.name || ""}
-                          invalid={
-                              validation.touched.name && validation.errors.name ? true : false
-                          }
-                      />
-                      {validation.touched.name && validation.errors.name ? (
-                          <FormFeedback type="invalid">{validation.errors.name}</FormFeedback>
-                      ) : null}
-                      </Col>
-                      <Col lg="6 mt-2">
-                      <Input
-                            name="email"
-                            className=""
-                            placeholder="Your email*"
-                            type="email"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            value={validation.values.email || ""}
-                            invalid={
-                                validation.touched.email && validation.errors.email ? true : false
-                            }
-                        />
-                        {validation.touched.email && validation.errors.email ? (
-                            <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
-                        ) : null}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg="12 mt-2">
-                      <Input
-                            name="subject"
-                            className=""
-                            placeholder="Your Subject.."
-                            type="text"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            value={validation.values.subject || ""}
-                            invalid={
-                                validation.touched.subject && validation.errors.subject ? true : false
-                            }
-                        />
-                        {validation.touched.subject && validation.errors.subject ? (
-                            <FormFeedback type="invalid">{validation.errors.subject}</FormFeedback>
-                        ) : null}
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg="12 mt-2">
-                        <div className="form-group">
-                          <textarea
-                            name="comments"
-                            id="comments"
-                            rows="4"
-                            className="form-control"
-                            placeholder="Your message..."
-                          ></textarea>
+            <div className="">
+            <Col lg="4" className="mt-4">
+                    <div className="services-box text-center hover-effect " >
+                      <div className="d-flex">
+                        <i className="pe-7s-mail text primary"></i>
+                        <div className="ms-4">
+                          <h4>Message Us At</h4>
+                          <p className="pt-2 text-muted">Email: admin@lifeisok.info </p>
                         </div>
-                      </Col>
-                    </Row>
-                    <Row>
-                      <Col lg="12" className="text-end">
-                        <Button className="submitBnt btn btn-primary">
-                          Send Message
-                        </Button>
-                        <div id="simple-msg"></div>
-                      </Col>
-                    </Row>
-                  </Form>
-                </div>
-              </Col>
-            </Row>
+                      </div>
+                    </div>
+                    
+                  </Col>
+                  <Col lg="4" className="mt-4">
+                    <div className="services-box text-center hover-effect " >
+                      <div className="d-flex">
+                        <i className="pe-7s-map text primary"></i>
+                        <div className="ms-4">
+                          <h4>Location</h4>
+                          <p className="pt-2 text-muted">Address: Shop 16 (Rapid Creek Shopping Village) 48 Trower Road, Milner, NT 0810
+                          Phone number: 0414787572
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </Col>
+
+            </div>
+
+          
           </Container>
         </section>
       </React.Fragment>
