0cb6617ab76368d230c0a4e16c36ef515d3db418
Abinash <avinashniure026@gmail.com>
2024-02-23 09:12:00 +0930

    formate change

--- src/components/Contact Us/contact-us.js
+++ src/components/Contact Us/contact-us.js
@@ -1,62 +1,68 @@
 import React from "react";
-import { Container, Row, Col, Button, FormFeedback, Form, Input } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  FormFeedback,
+  Form,
+  Input,
+} from "reactstrap";
 
 import { Link } from "react-router-dom";
-
 
 //Import Section Title
 import SectionTitle from "../common/section-title";
 
 const ContactUs = () => {
-    return (
-      <React.Fragment>
-        <section className="section " id="contact">
-          <Container>
-            {/* Render section title */}
-            <SectionTitle
-              title="Get In Touch"
-              description="We thrive when coming up with innovative ideas but also understand that a smart concept should be supported with faucibus sapien odio measurable results."
-            />
-            <div className="">
+  return (
+    <React.Fragment>
+      <section className="section " id="contact">
+        <Container>
+          {/* Render section title */}
+          <SectionTitle
+            title="Get In Touch"
+            description="We thrive when coming up with innovative ideas but also understand that a smart concept should be supported with faucibus sapien odio measurable results."
+          />
+          <div className="">
             <Col lg="4" className="mt-4">
-                    <div className="services-box text-center hover-effect " >
-                      <div className="d-flex">
-                        <i className="pe-7s-mail text primary"></i>
-                        <div className="ms-4">
-                          <h4>Message Us At</h4>
-                          <p className="pt-2 text-muted">Email: admin@lifeisok.info </p>
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </Col>
-                  <Col lg="4" className="mt-4">
-                    <div className="services-box text-center hover-effect " >
-                      <div className="d-flex">
-                        <i className="pe-7s-map text primary"></i>
-                        <div className="ms-4">
-                          <h4>Location</h4>
-                          <p className="pt-2 text-muted">Address: Shop 16 (Rapid Creek Shopping Village) 48 Trower Road, Milner, NT 0810
-                          Phone number: 0414787572
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </Col>
-
-            </div>
-            <Link
-                  to="/index1"
-                  className="btn btn-light ml-50 waves-effect waves-light"
-                >
-                  Contact Us
-                </Link>
-
-          
-          </Container>
-        </section>
-      </React.Fragment>
-    );
-}
+              <div className="services-box text-center hover-effect ">
+                <div className="d-flex">
+                  <i className="pe-7s-mail text primary"></i>
+                  <div className="ms-4">
+                    <h4>Message Us At</h4>
+                    <p className="pt-2 text-muted">
+                      Email: admin@lifeisok.info{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col lg="4" className="mt-4">
+              <div className="services-box text-center hover-effect ">
+                <div className="d-flex">
+                  <i className="pe-7s-map text primary"></i>
+                  <div className="ms-4">
+                    <h4>Location</h4>
+                    <p className="pt-2 text-muted">
+                      Address: Shop 16 (Rapid Creek Shopping Village) 48 Trower
+                      Road, Milner, NT 0810 Phone number: 0414787572
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </div>
+          <Link
+            to="/index1"
+            className="btn btn-light ml-50 waves-effect waves-light"
+          >
+            Contact Us
+          </Link>
+        </Container>
+      </section>
+    </React.Fragment>
+  );
+};
 
 export default ContactUs;
--- src/components/Website Description/website-description.js
+++ src/components/Website Description/website-description.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import { Container, Row, Col } from "reactstrap";
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Form from 'react-bootstrap/Form';
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Form from "react-bootstrap/Form";
 
 function DemoModal(props) {
   return (
@@ -14,17 +14,16 @@ function DemoModal(props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      
-                <Tabs
-                defaultActiveKey="profile"
-                id="uncontrolled-tab-example"
-                className="mb-3"
-              >
-                <Tab eventKey="home" title="Home">
-                <Container>
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home">
+          <Container>
             <Row>
               <Col lg="12" className="text-center"></Col>
-                <Form class="col text-center">
+              <Form class="col text-center">
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control type="email" placeholder="Enter email" />
@@ -37,24 +36,33 @@ function DemoModal(props) {
                   <Form.Label>Password</Form.Label>
                   <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
                   <Form.Label>Type your message here</Form.Label>
                   <Form.Control as="textarea" rows={3} />
                 </Form.Group>
-                <div><Button className="position-absolute top-90 start-50 translate-middle" variant="primary" type="submit">
-                  Submit
-                </Button></div>
+                <div>
+                  <Button
+                    className="position-absolute top-90 start-50 translate-middle"
+                    variant="primary"
+                    type="submit"
+                  >
+                    Submit
+                  </Button>
+                </div>
               </Form>
-              </Row>
-              </Container>
-                </Tab>
-                <Tab eventKey="profile" title="Profile">
-                  Tab content for Profile
-                </Tab>
-                <Tab eventKey="contact" title="Contact" disabled>
-                  Tab content for Contact
-                </Tab>
-              </Tabs>
+            </Row>
+          </Container>
+        </Tab>
+        <Tab eventKey="profile" title="Profile">
+          Tab content for Profile
+        </Tab>
+        <Tab eventKey="contact" title="Contact" disabled>
+          Tab content for Contact
+        </Tab>
+      </Tabs>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
@@ -62,35 +70,31 @@ function DemoModal(props) {
   );
 }
 
-function WebsiteDescription () {
-    const [modalShow, setModalShow] = React.useState(false);
-    return (
-      <React.Fragment>
-        <section className="section">
-          <div className="bg-overlay"></div>
-          <Container>
-            <Row>
-              <Col lg="12" className="text-center">
-                <h2 className="text-white">Join Our Team Today</h2>
-                <p className="pt-3 home-desc mx-auto">
-                Founded by a team of professionals to improve the quality of life for people with difficulties.
-                </p>
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-        Join Us
-      </Button>
+function WebsiteDescription() {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <React.Fragment>
+      <section className="section">
+        <div className="bg-overlay"></div>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="text-white">Join Our Team Today</h2>
+              <p className="pt-3 home-desc mx-auto">
+                Founded by a team of professionals to improve the quality of
+                life for people with difficulties.
+              </p>
+              <Button variant="primary" onClick={() => setModalShow(true)}>
+                Join Us
+              </Button>
 
-      <DemoModal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
-        
-              </Col>
-            </Row>
-          </Container>
-        </section>
-      </React.Fragment>
-    );
-  }
-
+              <DemoModal show={modalShow} onHide={() => setModalShow(false)} />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </React.Fragment>
+  );
+}
 
 export default WebsiteDescription;
